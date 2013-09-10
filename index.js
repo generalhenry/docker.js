061e6e226d37816bbe08a3f22335014164033b72
Henry Allen-Tilford <htilford@gmail.com>
2013-09-09 19:19:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ module.exports = function(opts) {
             path += '?' + querystring.stringify(opts.queryParams)
             delete opts.queryParams
           }
+          
           var url = resolve(host, path)
           request({url: url, json: opts, method: method}, function(err, res, json) {
             return checkStatus(err, res, json, cb)
@@ -100,139 +101,139 @@ module.exports = function(opts) {
     return {
       // CONTAINER
       listContainers: genFunction({
-        path: '/containers/json',
+        path: 'containers/json',
         method: 'GET',
         codes: {200:true, 400:"bad parameter", 500:"server error"}
       }),
       createContainer: genFunction({
-        path: '/containers/create',
+        path: 'containers/create',
         method: 'POST',
         codes: {201:true, 404:"no such image", 500:"server error"}
       }),
       inspectContainer: genFunction({
-        path: '/containers/{{id}}/json',
+        path: 'containers/{{id}}/json',
         method: 'GET',
         codes: {200:true, 404:"no such container", 500:"server error"}
       }),
       inspectContainerChanges: genFunction({
-        path: '/containers/{{id}}/changes',
+        path: 'containers/{{id}}/changes',
         method: 'GET',
         codes: {200:true, 404:"no such container", 500:"server error"}
       }),
       exportContainer: genFunction({
-        path: '/containers/{{id}}/export',
+        path: 'containers/{{id}}/export',
         method: 'GET',
         codes: {200:true, 404:"no such container", 500:"server error"}
       }),
       startContainer: genFunction({
-        path: '/containers/{{id}}/start',
+        path: 'containers/{{id}}/start',
         method: 'POST',
         codes: {200:true, 204:true, 404:"no such container", 500:"server error"}
       }),
       stopContainer: genFunction({
-        path: '/containers/{{id}}/stop',
+        path: 'containers/{{id}}/stop',
         method: 'POST',
         codes: {200:true, 204:true, 404:"no such container", 500:"server error"}
       }),
       restartContainer: genFunction({
-        path: '/containers/{{id}}/restart',
+        path: 'containers/{{id}}/restart',
         method: 'POST',
         codes: {204:true, 404:"no such container", 500:"server error"}
       }),
       killContainer: genFunction({
-        path: '/containers/{{id}}/kill',
+        path: 'containers/{{id}}/kill',
         method: 'POST',
         codes: {204:true, 404:"no such container", 500:"server error"}
       }),
       attachToContainer: genFunction({
-        path: '/containers/{{id}}/attach',
+        path: 'containers/{{id}}/attach',
         method: 'POST',
         codes: {200:true, 400:"bad parameter", 404:"no such container", 500:"server error"}
       }),
       waitContainer: genFunction({
-        path: '/containers/{{id}}/wait',
+        path: 'containers/{{id}}/wait',
         method: 'POST',
         codes: {200:true, 404:"no such container", 500:"server error"}
       }),
       removeContainer: genFunction({
-        path: '/containers/{{id}}',
+        path: 'containers/{{id}}',
         method: 'DELETE',
         codes: {204:true, 400:"bad parameter", 404:"no such container", 500:"server error"}
       }),
       // IMAGES
       listImages: genFunction({
-        path: '/images/json',
+        path: 'images/json',
         method: 'GET',
         codes: {200:true, 400:"bad parameter", 500:"server error"}
       }),
       createImage: genFunction({
-        path: '/images/create',
+        path: 'images/create',
         method: 'POST',
         codes: {200:true, 400:"bad parameter", 500:"server error"}
       }),
       insertFileImagecodes: genFunction({
-        path: '/images/{{id}}/insert',
+        path: 'images/{{id}}/insert',
         method: 'POST',
         codes: {200:true, 400:"bad parameter", 500:"server error"}
       }),
       inspectImage: genFunction({
-        path: '/images/{{id}}/json',
+        path: 'images/{{id}}/json',
         method: 'GET',
         codes: {200:true, 404:"no such image", 500:"server error"}
       }),
       historyImage: genFunction({
-        path: '/images/{{id}}/history',
+        path: 'images/{{id}}/history',
         method: 'GET',
         codes: {200:true, 404:"no such image", 500:"server error"}
       }),
       pushImage: genFunction({
-        path: '/images/{{id}}/push',
+        path: 'images/{{id}}/push',
         method: 'POST',
         codes: {200:true, 400:"bad parameter", 404:"no such image", 500:"server error"}
       }),
       tagImage: genFunction({
-        path: '/images/{{id}}/tag',
+        path: 'images/{{id}}/tag',
         method: 'POST',
         codes: {200:true, 400:"bad parameter", 404:"no such image", 500:"server error"}
       }),
       removeImage: genFunction({
-        path: '/images/{{id}}',
+        path: 'images/{{id}}',
         method: 'DELETE',
         codes: {200:true, 404:"no such image", 500:"server error"}
       }),
       searchImages: genFunction({
-        path: '/images/search',
+        path: 'images/search',
         method: 'GET',
         codes: {200:true, 500:"server error"}
       }),
       // MISC
       build: genFunction({
-        path: '/build',
+        path: 'build',
         method: 'POST',
         codes: {200:true, 500:"server error"}
       }),
       getAuth: genFunction({
-        path: '/auth',
+        path: 'auth',
         method: 'GET',
         codes: {200:true, 500:"server error"}
       }),
       setAuth: genFunction({
-        path: '/auth',
+        path: 'auth',
         method: 'POST',
         codes: {200:true, 204:true, 500:"server error"}
       }),
       systemInfo: genFunction({
-        path: '/info',
+        path: 'info',
         method: 'GET',
         codes: {200:true, 500:"server error"}
       }),
       version: genFunction({
-        path: '/version',
+        path: 'version',
         method: 'GET',
         codes: {200:true, 500:"server error"}
       }),
       commit: genFunction({
-        path: '/commit',
+        path: 'commit',
         method: 'POST',
         codes: {201:true, 404:"no such container", 500:"server error"}
       })
